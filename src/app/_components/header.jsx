7d12246447f1ba96ac478cc1ea2f8c2330709660
rkm10 <rajkumarmalluri08@gmail.com>
2025-01-09 13:07:06 +0530

    :minor sixes

--- src/app/_components/header.jsx
+++ src/app/_components/header.jsx
@@ -262,7 +262,7 @@ function Content() {
         </div>
 
         {/* use this div for other Experience */}
-        {/* <div>
+        <div>
           <ol className='group/list'>
             <li className='mb-12'>
               <div className='group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50'>
@@ -310,7 +310,7 @@ function Content() {
             </li>
 
           </ol>
-        </div> */}
+        </div>
 
       </section>
 

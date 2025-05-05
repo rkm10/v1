182fa64d1b9267ef068f0830c517f6891677591d
rkm10 <rajkumarmalluri08@gmail.com>
2025-05-05 18:11:36 +0530

    upadted ..

--- src/app/_components/header.jsx
+++ src/app/_components/header.jsx
@@ -283,7 +283,7 @@ function Content() {
 
         <div className='custom-cursor-default-hover'>
           <ul className='group/list custom-cursor-default-hover'>
-            {/* Anime Discovery App */}
+            {/* Luzoroffy */}
             <li className='mb-12'>
               <div className='group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100
                  lg:group-hover/list:opacity-50'>
@@ -293,13 +293,13 @@ function Content() {
                   <h3>
                     <Link
                       className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 group/link text-base"
-                      href="https://github.com/rkm10/ChatWidget"
+                      href="https://luzoroffy.vercel.app/"
                       target="_blank"
                       rel="noreferrer noopener"
                       aria-label="ChatWidget Project (opens in a new tab)"
                     >
                       <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block custom-cursor-on-hover"></span>
-                      <span>Anime Discovery App - PWA for Anime & Manga Info
+                      <span>Luzoroffy - Anime Discovery App
                         <span className="inline-block">
                           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px" aria-hidden="true">
                             <path fillRule="evenodd" d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" clipRule="evenodd">
@@ -320,14 +320,14 @@ function Content() {
                 </div>
                 <img
                   alt="Anime Discovery App"
-                  src="/api/placeholder/100/500"
+                  src="../images/raj/luzororffy-screenshot.webp"
                   className="aspect-video object-cover rounded border-2 border-slate-200/10 transition sm:order-1 sm:col-span-2 shadow-lg  group-hover:scale-105 sm:translate-y-1"
                 />
               </div>
             </li>
 
             {/* Real Estate Website */}
-            <li className='mb-12'>
+            {/* <li className='mb-12'>
               <div className='group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100
                  lg:group-hover/list:opacity-50'>
                 <div className='absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 
@@ -367,7 +367,7 @@ function Content() {
                   className="aspect-video object-cover rounded border-2 border-slate-200/10 transition sm:order-1 sm:col-span-2 shadow-lg  group-hover:scale-105 sm:translate-y-1"
                 />
               </div>
-            </li>
+            </li> */}
 
 
             {/* Session Tracker */}
@@ -407,7 +407,7 @@ function Content() {
                 </div>
                 <img
                   alt="Real Estate Website"
-                  src="/api/placeholder/100/500"
+                  src="../images/raj/breakreminder-screenshot.webp"
                   className="aspect-video object-cover rounded border-2 border-slate-200/10 transition sm:order-1 sm:col-span-2 shadow-lg  group-hover:scale-105 sm:translate-y-1"
                 />
               </div>
@@ -423,7 +423,7 @@ function Content() {
                   <h3>
                     <Link
                       className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 group/link text-base"
-                      href="https://github.com/rkm10/ChatWidget"
+                      href="https://my-airbnb-clone-raj.vercel.app/"
                       target="_blank"
                       rel="noreferrer noopener"
                       aria-label="ChatWidget Project (opens in a new tab)"
@@ -450,7 +450,7 @@ function Content() {
                 </div>
                 <img
                   alt="Real Estate Website"
-                  src="/api/placeholder/100/500"
+                  src="../images/raj/airbnb-screenshot.webp"
                   className="aspect-video object-cover rounded border-2 border-slate-200/10 transition sm:order-1 sm:col-span-2 shadow-lg  group-hover:scale-105 sm:translate-y-1"
                 />
               </div>

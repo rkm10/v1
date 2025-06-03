060c5eace98d35b14ee267c3bf995fb51ede7d05
rkm10 <rajkumarmalluri08@gmail.com>
2025-06-03 12:53:18 +0530

    updated logo

--- src/app/_components/header.jsx
+++ src/app/_components/header.jsx
@@ -52,7 +52,7 @@ function Header({ activeSection }) {
     <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
       <div>
         <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">
-          <Link href="/">Malluri Raj Kumar</Link>
+          <Link href="/">Malluri Raj Kumar </Link>
         </h1>
         <h2 className="mt-3 text-lg font-medium tracking-tight text-slate-200 sm:text-xl">
           Front End Engineer
